9908ce10dab1aa33d53e4f3b7a2c03a67d156d99
Valeriy V Nepluev <greenet2007@gmail.com>
2021-03-14 11:09:46 +0300

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if ((!Array.isArray(array)) || (array.length == 0)) {
+    return 0;
+  }
+
+  let aMin = array[0];
+  array.forEach(element => {
+      if (aMin > element) {
+          aMin = element;
+      }
+  });
+
+  return aMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((!Array.isArray(array)) || (array.length == 0)) {
+    return 0;
+  }
+
+  let aMax = array[0];
+  array.forEach(element => {
+      if (aMax < element) {
+          aMax = element;
+      }
+  });
+
+  return aMax;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if ((!Array.isArray(array)) || (array.length == 0)) {
+    return 0;
+  }
+
+    let aSum = 0;
+    array.forEach(element => {
+        aSum += element;
+    });
+
+    aSum = aSum / array.length;
+
+
+  return aSum.toFixed(2);
 }
